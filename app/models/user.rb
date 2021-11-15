db75dbed3d49e58ac22656b265efa7ec4fd25b79
Ahmed Tarabia <ahmedtarabia@gmail.com>
2021-11-15 17:33:32 -0500

    add user validation spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   def name
     "#{first_name} #{last_name}"
   end
+  validates :email, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :password_confirmation, presence: true
+  validates_uniqueness_of :email, case_sensitive: false
 end
